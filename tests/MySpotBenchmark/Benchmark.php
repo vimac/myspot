578a26d3c35aefffbda5ecab50f848c20b01f0f5
vimac <vifix.mac@gmail.com>
2020-02-20 11:32:58 +0800

    test recent versions

--- tests/MySpotBenchmark/Benchmark.php
+++ tests/MySpotBenchmark/Benchmark.php
@@ -3,14 +3,18 @@
 
 namespace MySpotBenchmark;
 
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use MySpot\SqlMap;
 use MySpot\SqlMapConfig;
 use MySpotTests\InMemoryDBProvider;
+use PDO;
 use PHPUnit\Framework\TestCase;
 
 /**
  * Class Benchmark
+ *
+ * No doubt it's much slower than native PDO, use it at your own risk
  */
 class Benchmark extends TestCase
 {
@@ -19,47 +23,88 @@ class Benchmark extends TestCase
 
     use InMemoryDBProvider;
 
-    public function test()
+    /**
+     * @test
+     * @dataProvider multipleTimes
+     * @group bench
+     */
+    public function bench()
     {
         $pdo = $this->inMemoryPDOProvider();
-        echo "No doubt it's much slower than native PDO, use it at your own risk\n";
-        for ($round = 0; $round < 3; $round++) {
-            echo "Round $round:\n";
-            $time = microtime(true);
-            $date = date('Y-m-d H:i:s');
-            for ($i = 0; $i < self::LEVEL; $i++) {
-                $pdo->exec(
-                    sprintf(
-                        "INSERT INTO `user` (`name`, `gender`, `created_at`) VALUES ('%s', %s, '%s')",
-                        "TEST" . $i,
-                        1,
-                        $date
-                    )
-                );
-            }
+        $pdo->exec("DELETE FROM `user`");
 
-            echo 'Insert by direct PDO ' . self::LEVEL . ' rows: ' . (microtime(true) - $time) . 's';
-            echo "\n";
+        $time = microtime(true);
+        $this->insertDirect($pdo, self::LEVEL);
+        printf("\nInsert directly %s rows: %ss \n", self::LEVEL, microtime(true) - $time);
+        $count = $this->countAndClear($pdo);
+        $this->assertEquals(self::LEVEL, (int)$count);
 
-            $path = __DIR__ . '/../assets';
-            $time = microtime(true);
-            $date = date('Y-m-d H:i:s');
-            $sqlMapConfig = new SqlMapConfig($path, $pdo, null, false);
-            $sqlMap = new SqlMap($sqlMapConfig);
-            for ($i = 0; $i < self::LEVEL; $i++) {
-                $sqlMap->insert('config.insert.insert2', [
-                        'name' => ['TEST' . $i],
-                        'gender' => [1],
-                        'createdAt' => [$date]
-                    ]
-                );
-            }
+        $time = microtime(true);
+        $this->insertByMySpot($pdo, self::LEVEL);
+        printf("\nInsert by SQLMap %s rows: %ss \n", self::LEVEL, microtime(true) - $time);
+        $count = $this->countAndClear($pdo);
+        $this->assertEquals(self::LEVEL, (int)$count);
+    }
 
-            echo 'Insert by SQLMap ' . self::LEVEL . ' rows: ' . (microtime(true) - $time) . 's';
-            echo "\n";
-        }
-
+    /**
+     * @test
+     * @group profile
+     */
+    public function profile()
+    {
+        $pdo = $this->inMemoryPDOProvider();
+        $this->insertByMySpot($pdo);
         $this->assertTrue(true);
+    }
+
+    private function insertDirect(PDO $pdo, $level = 1)
+    {
+        $date = date('Y-m-d H:i:s');
+        for ($i = 0; $i < $level; $i++) {
+            $stmt = $pdo->prepare(
+                sprintf(
+                    "INSERT INTO `user` (`name`, `gender`, `created_at`) VALUES ('%s', %s, '%s')",
+                    "TEST" . $i,
+                    1,
+                    $date
+                )
+            );
+            $stmt->execute();
+        }
+    }
+
+    private function insertByMySpot(PDO $pdo, $level = 1)
+    {
+        $path = __DIR__ . '/../assets';
+        $date = date('Y-m-d H:i:s');
+        $sqlMapConfig = new SqlMapConfig($path, $pdo, null, false);
+        $sqlMap = new SqlMap($sqlMapConfig);
+        for ($i = 0; $i < $level; $i++) {
+            $sqlMap->insert('config.insert.insert2', [
+                    'name' => ['TEST' . $i],
+                    'gender' => [1],
+                    'createdAt' => [$date]
+                ]
+            );
+        }
+    }
+
+    public function multipleTimes()
+    {
+        return [[], [], []];
+    }
+
+    /**
+     * @param PDO $pdo
+     * @return int
+     */
+    private function countAndClear(PDO $pdo)
+    {
+        $stmt = $pdo->prepare("select count(*) from `user`");
+        $stmt->execute();
+        $count = $stmt->fetchColumn();
+        $pdo->exec("DELETE FROM `user`");
+        return (int)$count;
     }
 
 
